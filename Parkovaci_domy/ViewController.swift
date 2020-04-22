aacb4226ad9199b9466323f83e64164b12177311
Jan Menšík <jonny.mensik@gmail.com>
2020-04-22 23:09:30 +0200

    Initial Commit

---
+++ Parkovaci_domy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parkovaci_domy
+//
+//  Created by Jan Menšík on 22/04/2020.
+//  Copyright © 2020 Jan Menšík. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
